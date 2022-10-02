ca37f6ab1d67ac27ddc0aa0966ff964989095d5c
Kevin Ng <102876076+KevinNg2@users.noreply.github.com>
2022-10-02 16:12:50 -0700

    calling on live server

---
+++ server.js
@@ -0,0 +1,19 @@
+// declared imports for app to run 
+const express = require("express");
+const routes = require("./routes");
+
+// import sequelize connection
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: true}));
+
+app.use(routes);
+
+// sequelize models to the database then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App server live on port ${PORT}!`))
+});
